249fdab9578f9ae307ed863ef86cb47905524df0
kasi-iyo <snakeman143@gmail.com>
2020-07-04 10:04:57 +0900

    index.jsxファイル作成（統括）、Airline.js&Airline.jsファイル作成（ビューコンポーネント）

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Airline = () => {
+  return (
+    <div>this is the airlines#show view</div>
+  )
+}
+
+export default Airline
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airlines = () => {
+  return <div>this is the airlines#index view</div>
+}
+
+export default Airlines
